8d19631fe2137960830bf6ffd4b044472b8a9c73
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:51:17 +0530

    feat: add standard library module

---
+++ src/stdlib/__init__.py
@@ -0,0 +1,15 @@
+from .math import *
+from .list import *
+from .string import *
+from .utils import *
+
+__all__ = [
+    # math
+    'add', 'subtract', 'multiply', 'divide', 'power', 'sqrt', 'sin', 'cos', 'tan',
+    # list
+    'length', 'append', 'insert', 'remove', 'index_of', 'slice',
+    # string
+    'length', 'concat', 'substring', 'index_of', 'to_uppercase', 'to_lowercase',
+    # utils
+    'now', 'sleep', 'type_of', 'is_instance_of'
+]
